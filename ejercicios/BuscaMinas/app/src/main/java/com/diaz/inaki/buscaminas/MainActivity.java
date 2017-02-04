cd9e4371d5797885526fca7966147f9c511e4bac
inakidml <inaki.diaz@ikasle.egibide.org>
2017-02-04 17:30:13 +0100

    eliminado padding relative layout

--- ejercicios/BuscaMinas/app/src/main/java/com/diaz/inaki/buscaminas/MainActivity.java
+++ ejercicios/BuscaMinas/app/src/main/java/com/diaz/inaki/buscaminas/MainActivity.java
@@ -12,12 +12,13 @@ import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
+public static int tamanio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tamanio=16;
         Tablero t = new Tablero(1);
         anadirHijos();
 
@@ -33,22 +34,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     public void anadirHijos() {
         Point size = tamanioPantalla();
-        int width = (size.x / 16);
-        int height = (size.y/ 16);
-
+        int width = (size.x / tamanio);
+        int height = ((size.y-120)/ tamanio);           //correción por la barra de menú (+/- 120)
 
         GridLayout g = (GridLayout) findViewById(R.id.grid1);
 
+        // pruebas porque se salen los botones
         g.setUseDefaultMargins(false);
         g.setAlignmentMode(GridLayout.ALIGN_BOUNDS);
         g.setRowOrderPreserved(false);
 
-
-        g.setColumnCount(16);
-        g.setRowCount(16);
+        //rejilla de los botones
+        g.setColumnCount(tamanio);
+        g.setRowCount(tamanio);
 
         Button b;
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < (tamanio*tamanio); i++) {
             b = new Button(this);
             ViewGroup.LayoutParams params=new ViewGroup.LayoutParams(
                     width,
@@ -56,10 +57,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             b.setLayoutParams(params);
             b.setPadding(0,0,0,0);
-
+            b.setText(""+i);
             b.setId(View.generateViewId());//identificador único
             b.setOnClickListener(this);
-            b.setText(""+(i+1));
 
             g.addView(b, i);
         }
