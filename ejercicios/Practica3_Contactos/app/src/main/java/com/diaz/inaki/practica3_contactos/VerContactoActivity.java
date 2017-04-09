6f96051fb2fb60541c12277332626ccd313b5f0e
inakidml <inaki.diaz@ikasle.egibide.org>
2017-04-09 21:31:01 +0200

    Creada activity ver contacto

---
+++ ejercicios/Practica3_Contactos/app/src/main/java/com/diaz/inaki/practica3_contactos/VerContactoActivity.java
@@ -0,0 +1,13 @@
+package com.diaz.inaki.practica3_contactos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VerContactoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ver_contacto);
+    }
+}
