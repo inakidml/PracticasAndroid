8b5457c6e3fd696e7b16ce33ad0972cad7b55b4e
Iñaki <inaki.diaz@ikasle.egibide.org>
2017-05-11 21:32:58 +0200

    enredando

--- ejercicios/Actividad_13_Media_Player/app/src/main/java/com/diaz/inaki/actividad_13_media_player/MainActivity.java
+++ ejercicios/Actividad_13_Media_Player/app/src/main/java/com/diaz/inaki/actividad_13_media_player/MainActivity.java
@@ -4,25 +4,94 @@ import android.content.res.AssetFileDescriptor;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.MediaController;
 
 import java.io.IOException;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements MediaController.MediaPlayerControl{
+private MediaPlayer mediaPlayer;
+    private MediaController mc;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         AssetFileDescriptor resource = getResources().openRawResourceFd(R.raw.julia);
 
-        MediaPlayer mediaPlayer = new MediaPlayer();
+        mediaPlayer = new MediaPlayer();
+        mc = new MediaController(this);
 
         try {
             mediaPlayer.setDataSource(resource);
             mediaPlayer.prepare();
+            mc.setMediaPlayer(this);
+            mc.setAnchorView(findViewById(R.id.textView));
+
         } catch (IOException e) {
+        } catch (IllegalStateException e) {
         }
 
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mc.show(10);
+    }
+
+    @Override
+    public void start() {
         mediaPlayer.start();
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public int getDuration() {
+        return 0;
+    }
+
+    @Override
+    public int getCurrentPosition() {
+        return 0;
+    }
+
+    @Override
+    public void seekTo(int i) {
+
+    }
+
+    @Override
+    public boolean isPlaying() {
+        return false;
+    }
+
+    @Override
+    public int getBufferPercentage() {
+        return 0;
+    }
+
+    @Override
+    public boolean canPause() {
+        return false;
+    }
+
+    @Override
+    public boolean canSeekBackward() {
+        return false;
+    }
+
+    @Override
+    public boolean canSeekForward() {
+        return false;
+    }
+
+    @Override
+    public int getAudioSessionId() {
+        return 0;
     }
 }
